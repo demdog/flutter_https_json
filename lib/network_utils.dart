333c898591c518112ba5f15a5f11d6fa663c5196
demdo <demdog@hotmail.com>
2019-07-05 14:32:50 +0800

    https option parameters

--- lib/network_utils.dart
+++ lib/network_utils.dart
@@ -8,24 +8,28 @@ class NetworkUtils {
   static const TrustHost = 'test.apigj.com';
   static const Host = 'test.apigj.com:18443';
 
-  static httpsPostData(String urlPath, Object reqData) async {
+  static httpsPostData(String urlPath, Object reqData, {String host, Map<String, String> header, String trustHost, String certSubject, String certIssuer}) async {
     var httpClient = new HttpClient();
     httpClient.badCertificateCallback = (X509Certificate cert, String host, int port) {
-      if(cert.subject == CertSubject && cert.issuer == CertIssuer){
-        if(host == TrustHost){
+      if(cert.subject == (certSubject??CertSubject) && cert.issuer == (certIssuer??CertIssuer)){
+        if(host == (trustHost??TrustHost)){
           return true;
         }
       }
       return false;
     };
     // httpClient.addCredentials(url, realm, credentials)
-
-    var request = await httpClient.postUrl(new Uri.https(Host,urlPath));
-
+    var request = await httpClient.postUrl(new Uri.https(host??Host,urlPath));
+    if(header != null){
+      for (var k in header.entries) {
+        request.headers.set(k.key, k.value);
+      }
+    }
     request.add(utf8.encode(json.encode(reqData)));
     var response = await request.close();
     var responseBody = await response.transform(utf8.decoder).join();
-
+      
+    print(responseBody);
     Map dataMap = jsonDecode(responseBody);
     httpClient.close();
     return dataMap;
